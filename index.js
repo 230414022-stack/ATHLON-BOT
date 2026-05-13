47bf82f81f76d6143a22ebffdc0a8cf6552ac62f
jake dave <230414022@mhs.udb.ac.id>
2026-05-13 23:24:43 +0700

    bot pertama

---
+++ index.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+
+const TelegramBot = require("node-telegram-bot-api");
+const Groq = require("groq-sdk");
+
+// ======================
+// TOKEN
+// ======================
+
+const bot = new TelegramBot(
+  process.env.BOT_TOKEN,
+  {
+    polling: true,
+  }
+);
+
+// ======================
+// GROQ AI
+// ======================
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+// ======================
+// BOT START
+// ======================
+
+console.log("BOT NYALA 🚀");
+
+// ======================
+// CHAT HANDLER
+// ======================
+
+bot.on("message", async (msg) => {
+
+  const chatId = msg.chat.id;
+  const userMessage = msg.text;
+
+  if (!userMessage) return;
+
+  bot.sendChatAction(chatId, "typing");
+
+  try {
+
+    // kirim ke AI
+    const chatCompletion =
+      await groq.chat.completions.create({
+
+        messages: [
+          {
+            role: "user",
+            content: userMessage,
+          },
+        ],
+
+        model: "llama-3.3-70b-versatile",
+
+      });
+
+    // ambil jawaban AI
+    const aiResponse =
+      chatCompletion.choices[0]
+      .message.content;
+
+    // kirim ke telegram
+    await bot.sendMessage(
+      chatId,
+      aiResponse
+    );
+
+  } catch (error) {
+
+    console.log("ERROR:", error);
+
+    await bot.sendMessage(
+      chatId,
+      "AI lagi pusing bos 😭"
+    );
+  }
+});
